70ea0d420d4cbfd79a772412475d2404c6e55a1b
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2018-12-06 19:03:59 +0200

    add video support

--- src/content_script.js
+++ src/content_script.js
@@ -7,6 +7,9 @@ chrome.runtime.onMessage.addListener( (req, sender, res) => {
 })
 
 function img_url() {
-    let img = document.querySelector('div:not([class])[role="button"] img')
-    return img ? img.src : null
+    let div = 'div:not([class])[role="button"]'
+    for (let tag of ['video', 'img']) {
+	let el = document.querySelector(`${div} ${tag}`)
+	if (el && el.src) return el.src
+    }
 }
